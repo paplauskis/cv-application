e51cd521681b7b7111b724d4917efc3e213efd3d
paplauskis <paplauskaskostas@gmail.com>
2023-08-22 23:04:25 +0300

    removed labels and their divs

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,12 @@ export function PersonalInfo() {
     <div className="personal-info">
       <h2>Personal Info</h2>
       {showMore && <div className="labels-inputs">
-      <div className="input-div">
-        <label htmlFor="firstName">First Name:</label>
-        <input type="text" id='firstName' name='first_name'/>
-      </div>
-      <div className="input-div">
-        <label htmlFor="lastName">Last Name:</label>
-        <input type="text" id='lastName' name='last_name'/>
-      </div>
-      <div className="input-div">
-        <label htmlFor="title">Title:</label>
-        <input type="text" id='title' name='title'/>
-      </div>
-      <div className="input-div">
-        <label htmlFor="phoneNumber">Phone Number:</label>
-        <input type="tel" id='phoneNumber' name='phone_number'/>
-      </div>
-      <div className="input-div">
-        <label htmlFor="email">Email:</label>
-        <input type="email" id='email' name='email'/>
-      </div>
-      <div className="input-div">
-        <label htmlFor="about">About:</label>
-        <textarea name="about" id="about" cols="30" rows="10"></textarea>
-      </div>
+        <input type="text" id='firstName' name='first_name' placeholder='First Name'/>
+        <input type="text" id='lastName' name='last_name' placeholder='Last Name'/>
+        <input type="text" id='title' name='title' placeholder='Title'/>
+        <input type="tel" id='phoneNumber' name='phone_number' placeholder='Phone Number'/>
+        <input type="email" id='email' name='email' placeholder='Email'/>
+        <textarea name="about" id="about" cols="30" rows="10" placeholder='About'></textarea>
       <button type='submit' className='submit-button submit-personal-info'>Submit</button>
       </div>}
       <button className='show-hide-button' onClick={handleShowClick}>{showMore ? 'Hide' : 'Show'}</button>
