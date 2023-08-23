f869902c5d035102c98a29ef43d43c9fce1c2adb
paplauskis <paplauskaskostas@gmail.com>
2023-08-23 20:01:42 +0300

    added useState for email, phone

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 
-export function PersonalInfo({ setFirstName, setLastName, setTitle }) {
+export function PersonalInfo({ setFirstName, setLastName, setTitle, setEmail, setPhoneNumber, setAbout }) {
   const [showMore, setShowMore] = useState('true')
 
   function handleShowClick() {
@@ -16,8 +16,21 @@ export function PersonalInfo({ setFirstName, setLastName, setTitle }) {
   function handleLastName(e) {
     setLastName(e.target.value)
   }
+
   function handleTitle(e) {
     setTitle(e.target.value)
+  }
+
+  function handleEmail(e) {
+    setEmail(e.target.value)
+  }
+
+  function handlePhoneNumber(e) {
+    setPhoneNumber(e.target.value)
+  }
+
+  function handleAbout(e) {
+    setAbout(e.target.value)
   }
 
   return (
@@ -27,9 +40,9 @@ export function PersonalInfo({ setFirstName, setLastName, setTitle }) {
         <input type="text" id='firstName' name='first_name' placeholder='First Name' onChange={handleFirstName}/>
         <input type="text" id='lastName' name='last_name' placeholder='Last Name' onChange={handleLastName}/>
         <input type="text" id='title' name='title' placeholder='Title' onChange={handleTitle}/>
-        <input type="tel" id='phoneNumber' name='phone_number' placeholder='Phone Number'/>
-        <input type="email" id='email' name='email' placeholder='Email'/>
-        <textarea name="about" id="about" cols="30" rows="10" placeholder='About'></textarea>
+        <input type="tel" id='phoneNumber' name='phone_number' placeholder='Phone Number' onChange={handlePhoneNumber}/>
+        <input type="email" id='email' name='email' placeholder='Email' onChange={handleEmail}/>
+        <textarea name="about" id="about" cols="30" rows="10" placeholder='About' onChange={handleAbout}></textarea>
       <button type='submit' className='submit-button submit-personal-info' >Submit</button>
       </div>}
       <button className='show-hide-button' onClick={handleShowClick}>{showMore ? 'Hide' : 'Show'}</button>
@@ -88,22 +101,32 @@ export function Container() {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [title, setTitle] = useState('')
+  const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [about, setAbout] = useState('')
   return (
     <>
       <div id="forms">
-        <PersonalInfo setFirstName={setFirstName} setLastName={setLastName} setTitle={setTitle}/>
+        <PersonalInfo 
+        setFirstName={setFirstName}
+        setLastName={setLastName}
+        setTitle={setTitle}
+        setEmail={setEmail}
+        setPhoneNumber={setPhoneNumber}
+        setAbout={setAbout}
+        />
         <EducationInfo />
         <ExperienceInfo />
       </div>
       <div id="cv-preview">
         <div id="cv-header">
           <div className="name-title-wrapper">
-            <h1 className='full-name'>{firstName + ' ' + lastName}</h1>
-            <h2 className='title'>{title}</h2>
+            <h2 className='full-name'>{firstName + ' ' + lastName}</h2>
+            <h2 className='title'><em>{title}</em></h2>
           </div>
           <div className="contact-wrapper">
-            <p><img src="../public/mail.png" alt="email icon" /></p>
-            <p><img src="../public/telephone.png" alt="phone icon" /></p>
+            <p><img src="../public/mail.png" alt="email icon" />{email}</p>
+            <p><img src="../public/telephone.png" alt="phone icon" />{phoneNumber}</p>
           </div>
         </div>
       </div>
